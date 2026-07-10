010d5dba45cbf0aa1f426673f91b2664efc1f373
UltralyticsAssistant <web@ultralytics.com>
2026-07-10 09:51:49 +0000

    Auto-format by https://ultralytics.com/actions

--- myxiugai/SaMam-main/ARCHI/VSSM.py
+++ myxiugai/SaMam-main/ARCHI/VSSM.py
@@ -1,22 +1,21 @@
-
 import torch.nn as nn
-from ARCHI.archi_utils import PatchEmbed,PatchUnEmbed
-
-
+from ARCHI.archi_utils import PatchEmbed, PatchUnEmbed
 from ARCHI.SS2D_Encoder import SS2D_encoder
 
 
 class VSSM(nn.Module):
     def __init__(
-            self,
-            hidden_dim: int = 64,
-            d_state: int = 16,
-            expand: float = 2.,
-            mamba_from_trion=1,
-            **kwargs,
+        self,
+        hidden_dim: int = 64,
+        d_state: int = 16,
+        expand: float = 2.0,
+        mamba_from_trion=1,
+        **kwargs,
     ):
         super().__init__()
-        self.SSM = SS2D_encoder(d_model=hidden_dim, d_state=d_state,expand=expand,mamba_from_trion=mamba_from_trion, **kwargs)
+        self.SSM = SS2D_encoder(
+            d_model=hidden_dim, d_state=d_state, expand=expand, mamba_from_trion=mamba_from_trion, **kwargs
+        )
 
         self.patch_embed = PatchEmbed()
         self.patch_unembed = PatchUnEmbed()
@@ -36,22 +35,15 @@ class VSSM(nn.Module):
         return x + input
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import torch
 
-    net = VSSM(hidden_dim=32,expand=2,representation_dim=32).cuda()
+    net = VSSM(hidden_dim=32, expand=2, representation_dim=32).cuda()
 
-    print('# net parameters:', sum(param.numel() for param in net.parameters()), '\n')
+    print("# net parameters:", sum(param.numel() for param in net.parameters()), "\n")
 
-    img = torch.randn((1,32,20,35)).cuda()
+    img = torch.randn((1, 32, 20, 35)).cuda()
     # print(img)
-    degra = torch.randn((1,32)).cuda()
+    degra = torch.randn((1, 32)).cuda()
     out = net.forward(img)
     print(out.shape)
-
-
-
-
-
-
